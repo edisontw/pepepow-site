43b36f99cae058af10a3d7dbcacb62cde91ca88e
Edison Huang <107197406+edisontw@users.noreply.github.com>
2026-09-19 21:32:31 +0800

    site: keep default markdown processor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
-import removeMigrationBanner from './scripts/markdown/remove-migration-banner.mjs';
 
 export default defineConfig({
   site: 'https://pepepow.net',
   output: 'static',
   trailingSlash: 'always',
-  markdown: {
-    remarkPlugins: [removeMigrationBanner],
-  },
 });
